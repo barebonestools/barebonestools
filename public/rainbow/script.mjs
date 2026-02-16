1ab936ad44f22c500494909d42590058c6093518
spao <spao@heritagechristianschool.com>
2026-02-16 23:36:34 +0000

    basically done; would like to add moving around feature

--- public/rainbow/script.mjs
+++ public/rainbow/script.mjs
@@ -1,33 +1,34 @@
 import util from "../_files_/util.mjs";
 util.betterand(10, 18);
+const onerun = (7 / 8) * 1000;
 let a = "";
-let set = []
+let set = [];
 setTimeout(() => {
     setInterval(() => {
-        
+        const plpr = util.betterand(7, 80)
+        const ptpb = util.betterand(15, 80)
         let temp = `    
         --btrr${a}: ${util.betterand(10, 50)}%;
         --btlr${a}: ${util.betterand(10, 50)}%;
         --bbrr${a}: ${util.betterand(10, 50)}%;
         --bblr${a}: ${util.betterand(10, 50)}%;
-        --pl${a}: ${util.betterand(15, 80)}px;
-        --pr${a}: ${util.betterand(15, 80)}px;
-        --pt${a}: ${util.betterand(15, 80)}px;
-        --pb${a}: ${util.betterand(15, 80)}px;
+        --pl${a}: ${plpr}px;
+        --pr${a}: ${plpr}px;
+        --pt${a}: ${ptpb}px;
+        --pb${a}: ${ptpb}px;
         --fs${a}: ${util.betterand(60, 120)}px;`;
         console.log("helloe");
         if (a == "") {
-            set[0]= temp
+            set[0] = temp;
             a = "2";
-        }
-        else if (a == "3") {
-            set[a-1]= temp
+        } else if (a == "4") {
+            set[a - 1] = temp;
             a = "";
         } else {
-            set[a-1]=temp
+            set[a - 1] = temp;
             a++;
         }
-        document.getElementById("abc").style.cssText = set.join("")
-    }, 875);
-}, 875);
+        document.getElementById("abc").style.cssText = set.join("");
+    }, onerun);
+}, onerun);
 //}, 437.5);
