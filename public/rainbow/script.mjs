4a14cb0b604862b2481e60aa9ca0fe4161988e72
spao <spao@heritagechristianschool.com>
2026-02-24 04:17:29 +0000

    moving ot of bounds?! (less but will still hapeen) also need to test new slower speed

--- public/rainbow/script.mjs
+++ public/rainbow/script.mjs
@@ -1,12 +1,12 @@
 import util from "../_files_/util.mjs";
 util.betterand(10, 18);
-const onerun = (7 / 8) * 1000;
+const onerun = (10 / 8) * 1000;
 let a = "";
 let set = [];
 setTimeout(() => {
     setInterval(() => {
-        const plpr = util.betterand(7, 80)
-        const ptpb = util.betterand(15, 80)
+        const plpr = util.betterand(7, 80);
+        const ptpb = util.betterand(15, 80);
         let temp = `    
         --btrr${a}: ${util.betterand(10, 50)}%;
         --btlr${a}: ${util.betterand(10, 50)}%;
@@ -17,8 +17,14 @@ setTimeout(() => {
         --pt${a}: ${ptpb}px;
         --pb${a}: ${ptpb}px;
         --fs${a}: ${util.betterand(60, 120)}px;
-        --mt${a}: ${util.betterand(10, window.innerHeight-(144.8+ptpb*2))}px;
-        --ml${a}: ${util.betterand(10, window.innerWidth-(484.2+plpr*2))}px;
+        --mt${a}: ${util.betterand(
+            0,
+            window.innerHeight - (144.8 + ptpb * 2)
+        )}px;
+        --ml${a}: ${util.betterand(
+            0,
+            window.innerWidth - (484.2 + plpr * 2)
+        )}px;
         `;
         console.log("helloe");
         if (a == "") {
